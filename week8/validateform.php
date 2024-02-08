bb6294bcfc279f8c1f0349669fc82445a484442f
Miguel-Moldez <aomoldez@student.apc.edu.ph>
2024-02-08 14:54:58 +0800

    Updated validateform.php

--- week8/validateform.php
+++ week8/validateform.php
@@ -109,8 +109,8 @@ if ($conn->connect_error) {
   die("Connection failed: " . $conn->connect_error);
 }
 
-$sql = "INSERT INTO aomoldez_myguests (firstname, lastname, email)
-VALUES ('$name', '$website', '$email')";
+$sql = "INSERT INTO aomoldez_myguests (name, email, website, comment, gender)
+VALUES ('$name', '$email', '$website', '$comment', '$gender)";
 
 if ($conn->query($sql) === TRUE) {
   echo "New record created successfully";
